573db15b27b47454937ac634e6ac44da96b85bd1
jacubas <jcubas@grupoplatino.hn>
2022-07-12 15:02:10 -0600

    Corregir errores en los formularios generados por la nueva dependencia.

--- src/components/detail_product/compare_specs.js
+++ src/components/detail_product/compare_specs.js
@@ -19,11 +19,11 @@ function CompareSpecs(props) {
                 <div className='bg-gray-100  rounded-xl'>
                     <button
                         type="button"
-                        className="relative block w-full h-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="relative block w-full h-full border-2 border-gray-300 border-dashed justify-center rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                        </svg>
                         <span className="mt-2 block text-sm font-medium text-gray-900">Create a new database</span>
                     </button>
                 </div>
