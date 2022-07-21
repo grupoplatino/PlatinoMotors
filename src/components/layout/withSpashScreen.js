ad49dd69ef75147679f8f2bf7236290afcde96a7
jacubas <jcubas@grupoplatino.hn>
2022-07-21 10:52:03 -0600

    Avances en el navbar para Tablets

--- src/components/layout/withSpashScreen.js
+++ src/components/layout/withSpashScreen.js
@@ -1,62 +1,53 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component} from 'react';
 import { HashLoader } from 'react-spinners';
 import { PlatinoMotors } from '../../assets';
 import ScrollToTop from '../../utils/scroll_to_top';
 function LoadingWelcomeMessage() {
     return (
-        <div className="bg-blackCustom-900 h-screen flex px-5 flex-col justify-center items-center">
+        <div className="bg-black bg-opacity-10 h-screen flex px-5 flex-col justify-center items-center">
             <div className='mb-8'>
                 <img src={PlatinoMotors} alt="Platino HN" className='w-72' />
             </div>
             <div>
-                <HashLoader color='#FFFFFF' height={10} width={200} />
+                <HashLoader color='#000000' height={10} width={200} />
             </div>
-            <div className='mt-8 text-white'>
+            <div className='mt-8 text-gray-500 font-pop font-semibold text-xl'>
                 Cargando...
             </div>
         </div>
     );
 };
-function withSpashScreen(WrappedComponent) {
-    const [isLoading, setIsLoading] = useState(true);
-     componentDidMount() 
-     {
-        try {
-         
-            useEffect(() => {
-                setTimeout(() => {
-                    setIsLoading(false);
-                }, 4000)
-                ScrollToTop();
-            }, []);
-            setTimeout(() => {
-                this.setState({
-                    loading: false,
-                })
-            })
-        } catch (err) {
-            console.log(err);
-            this.setState({
-                loading: false,
-            })
-        }
-    }
-    render()
-     {
-        if (this.state.loading) return LoadingWelcomeMessage();
-        return <WrappedComponent {...this.props} />
-    }
 
-
+function WithSplashScreen(WrappedComponent) {
     return class extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                loading: true,
-            }
+      constructor(props) {
+        super(props);
+        this.state = {
+          loading: true,
+        };
+      }
+  
+      async componentDidMount() {
+        try {
+         //TODO hacer llamada a la API (Cuando exista)
+          setTimeout(() => {
+            this.setState({
+              loading: false,
+            });
+          }, 10000)
+        } catch (err) {
+          console.log(err);
+          this.setState({
+            loading: false,
+          });
         }
-       
+      }
+  
+      render() {
+        if (this.state.loading) return LoadingWelcomeMessage();
+        return <WrappedComponent {...this.props} />;
+      }
     };
-};
+  }
 
-export default withSpashScreen;
+export default WithSplashScreen;
