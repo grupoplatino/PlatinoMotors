70a8a5b96082053dcffe931f4608de48016f7560
jacubas <jcubas@grupoplatino.hn>
2022-07-28 11:39:45 -0600

    Actualizado el navbar

--- src/components/make_your_own/body.js
+++ src/components/make_your_own/body.js
@@ -207,9 +207,9 @@ function Body(props) {
                                             <p className='font- font-medium text-black text-center'>El Mejor Costo-Beneficio</p>
                                         </div>
                                         <div className='flex justify-center'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-</svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                                            </svg>
                                         </div>
                                         <div>
                                             <p className='text-xs text-center'>
